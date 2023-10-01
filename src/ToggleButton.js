37f2ea252f9396dbd5de386dbbcbdd1a2463c030
james Agu <preagu@yahoo.com>
2023-10-01 05:43:35 +0100

    Changed handleClick to arrow function and so passed value to the it.

--- src/ToggleButton.js
+++ src/ToggleButton.js
@@ -6,19 +6,23 @@ class ToggleButton extends React.Component {
     this.state = {isToggleOn: true};
   }
 
-  handleClick = (e) => {
+  handleClick = (e, val) => {
     if(e){
       e.preventDefault();
     }
     this.setState((prevState) => {
       return {isToggleOn: prevState.isToggleOn ? false : true};
     });
+    console.log(val);
   };
 
   render() {
     return (
       <div>
-        <button onClick={this.handleClick}>{this.state.isToggleOn ? "On" : "Off"}</button>
+        <button onClick={(e) => {
+            this.handleClick(e, "Clicked Hello");
+          }
+        }>{this.state.isToggleOn ? "On" : "Off"}</button>
       </div>
     );
   }
